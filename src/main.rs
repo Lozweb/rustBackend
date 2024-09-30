f6e1e1be260d0877293024e7aff00f5d66808b6e
Durondil <aelozweb@gmail.com>
2024-09-30 15:55:03 +0200

    missing files

---
+++ src/main.rs
@@ -0,0 +1,75 @@
+use anyhow::Result;
+use axum::http::Method;
+use axum::{http, Extension, Router};
+use dotenvy::dotenv;
+use rustBackend::db::init_db;
+use rustBackend::error::handle_error;
+use rustBackend::user::user_router;
+use std::net::SocketAddr;
+use tower_http::cors::{Any, CorsLayer};
+use tower_http::trace;
+use tower_http::trace::TraceLayer;
+use tracing::Level;
+
+#[tokio::main]
+async fn main() {
+    configure_logger();
+    handle_error(try_main).await;
+}
+
+fn configure_logger() {
+    #[cfg(debug_assertions)]
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+
+    #[cfg(not(debug_assertions))]
+    tracing_subscriber::fmt()
+        .json()
+        .with_max_level(Level::INFO)
+        .with_current_span(false)
+        .init();
+}
+
+async fn try_main() -> Result<()> {
+    dotenv().ok();
+
+    
+    let db = init_db().await?;
+
+    let trace_layer = TraceLayer::new_for_http()
+        .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+        .on_response(trace::DefaultOnResponse::new().level(Level::INFO));
+
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE])
+        .allow_headers([
+            http::header::AUTHORIZATION,
+            http::header::CONTENT_TYPE,
+            http::header::CONTENT_LENGTH,
+        ])
+        .allow_origin(Any);
+
+    let app = Router::new()
+        .merge(user_router())
+        .layer(trace_layer)
+        .layer(cors)
+        .layer(Extension(db));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    tracing::info!("Listening on {}", addr);
+
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await?;
+
+    Ok(())
+}
+
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("expect tokio signal ctrl-c");
+}
